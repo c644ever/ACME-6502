cf949290081abbfafa4d4b653cc4116991b402d2
c644ever <c6443v3r@gmail.com>
2025-10-28 23:07:12 +0100

    update

--- lesson01/blink_serial/blink_serial.ino
+++ lesson01/blink_serial/blink_serial.ino
@@ -1,20 +1,20 @@
-//
+// Let the builin LED Blink and make some serial output
 
 #define LED	13
 
 void setup (void)
 {
-	pinMode(LED,OUTPUT);
-	Serial.begin(115200);
+	pinMode(LED,OUTPUT);		// Pin 13 which is connected to the onboard LED as OUTPUT
+	Serial.begin(115200);		// Begin serial communication with 115200 bits per second 
 }
 
 void loop ( void )
 {
-	digitalWrite(LED,HIGH);
-	Serial.println ( "LED an");
-	delay(500);
+	digitalWrite(LED,HIGH);		//switch the LED on
+	Serial.println ( "LED on");	//tell this over serial
+	delay(500);			//wait 500 milliseconds
 
-	digitalWrite(LED,LOW);
-	Serial.println ("LED aus");
+	digitalWrite(LED,LOW);		//you find out yourself ;-)
+	Serial.println ("LED off");
 	delay(500);
 }
